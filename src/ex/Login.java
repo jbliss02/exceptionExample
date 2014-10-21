4ad4d994458abcaf2fe80e2772421bd14bf8608f
jbliss02 <james.bliss@outlook.com>
2014-10-21 19:35:43 +0100

    initial

--- src/ex/Login.java
+++ src/ex/Login.java
@@ -12,7 +12,7 @@ public class Login {
 		{
 			throw new SecurityBreachException();
 			
-		}
+		}//
 	}
 	
 	public static void main(String[] args)
--- src/ex/SecurityBreachException.java
+++ src/ex/SecurityBreachException.java
@@ -5,5 +5,5 @@ public class SecurityBreachException extends Exception {
 	public SecurityBreachException(){
 		super("Attempted Security Breach - User attempted to breach security");
 	}
-	
+	//
 }
